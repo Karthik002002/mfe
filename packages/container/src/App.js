ca2be2bd15764ab68b2bad1fc96c8c4970658130
Karthik <karthikpanda002@gmail.com>
2025-03-23 08:24:02 +0530

    CSS autogeneration conflict solved

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -2,14 +2,18 @@ import React from "react";
 import MarketingApp from "./components/MarketingApp";
 import Header from "./components/Header";
 import { BrowserRouter } from "react-router-dom";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
 export default function App() {
+  const generateClassName = createGenerateClassName({ productionPrefix: "co" });
   return (
-    <BrowserRouter>
-      <div>
-        <Header />
-        <MarketingApp />
-      </div>
-    </BrowserRouter>
+    <StylesProvider generateClassName={generateClassName}>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <MarketingApp />
+        </div>
+      </BrowserRouter>
+    </StylesProvider>
   );
 }
--- packages/marketing/src/App.js
+++ packages/marketing/src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
 import { Switch, Route, BrowserRouter } from "react-router-dom";
-import { StylesProvider } from "@material-ui/core";
+import { StylesProvider, createGenerateClassName } from "@material-ui/core";
 
 import Landing from "./components/Landing";
 import Pricing from "./components/Pricing";
 
 export default () => {
-  console.log("Marketing App");
+  const generateClassName = createGenerateClassName({ productionPrefix: "ma" });
 
   return (
     <div>
-      <StylesProvider>
+      <StylesProvider generateClassName={generateClassName}>
         <BrowserRouter>
           <Switch>
             <Route exact path={"/pricing"} component={Pricing} />
